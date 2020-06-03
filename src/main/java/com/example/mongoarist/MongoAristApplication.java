664f3d9c789cece8ad3d9b91c972c38ac9c813c6
arist <18271462455@163.com>
2020-06-03 10:35:27 +0800

    mongodb first commit

---
+++ src/main/java/com/example/mongoarist/MongoAristApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongoarist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoAristApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoAristApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mongoarist/MongoAristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongoarist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoAristApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
